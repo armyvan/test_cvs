c1daf712d52f153491c093c20f9c52f99f8e8d45
armyvan <rubiomurilloivan@gmail.com>
2025-01-22 17:49:15 +0100

    Cambio en linea 1

--- Proyecto.java
+++ Proyecto.java
@@ -1,5 +1,5 @@
 public class Proyecto{
     public static void main(String[] args) {
-        System.out.println("Viva España coño, fuera moros de España");
+        System.out.println("Viva España coño, fuera moros de España. Viva la guardia Civil");
     }
 }
